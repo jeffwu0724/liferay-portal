31903a081775737d3835fb1a1371b75917597b12
Jiefeng Wu <jeffwu0724@gmail.com>
2023-03-08 13:37:27 -0800

    remove optional usage in ContentFieldUtil

--- modules/apps/headless/headless-delivery/headless-delivery-api/src/main/java/com/liferay/headless/delivery/dto/v1_0/util/ContentFieldUtil.java
+++ modules/apps/headless/headless-delivery/headless-delivery-api/src/main/java/com/liferay/headless/delivery/dto/v1_0/util/ContentFieldUtil.java
@@ -58,10 +58,10 @@ import java.util.List;
 import java.util.Locale;
 import java.util.Map;
 import java.util.Objects;
-import java.util.Optional;
 import java.util.TimeZone;
 
 import javax.ws.rs.BadRequestException;
+import javax.ws.rs.core.MultivaluedMap;
 import javax.ws.rs.core.UriInfo;
 
 /**
@@ -114,14 +114,13 @@ public class ContentFieldUtil {
 						}
 
 						Map<String, ContentFieldValue> map = new HashMap<>();
+						Value value = ddmFormFieldValue.getValue();
 
-						Map<Locale, String> valueValues = Optional.ofNullable(
-							ddmFormFieldValue.getValue()
-						).map(
-							Value::getValues
-						).orElse(
-							Collections.emptyMap()
-						);
+						Map<Locale, String> valueValues = new HashMap<>();
+
+						if (value.getValues() != null) {
+							valueValues = value.getValues();
+						}
 
 						for (Map.Entry<Locale, String> entry :
 								valueValues.entrySet()) {
@@ -448,20 +447,14 @@ public class ContentFieldUtil {
 			long classPK, DTOConverterContext dtoConverterContext)
 		throws Exception {
 
-		Optional<UriInfo> uriInfoOptional =
-			dtoConverterContext.getUriInfoOptional();
+		UriInfo uriInfo = dtoConverterContext.getUriInfo();
 
-		if (uriInfoOptional.map(
-				UriInfo::getQueryParameters
-			).map(
-				queryParameters -> queryParameters.getFirst("nestedFields")
-			).map(
-				nestedFields -> nestedFields.contains(
-					"embeddedStructuredContent")
-			).orElse(
-				false
-			)) {
+		MultivaluedMap<String, String> queryParameters =
+			uriInfo.getQueryParameters();
 
+		String nestedField = queryParameters.getFirst("nestedFields");
+
+		if (nestedField.contains("embeddedStructuredContent")) {
 			DTOConverterRegistry dtoConverterRegistry =
 				dtoConverterContext.getDTOConverterRegistry();
 
@@ -478,8 +471,7 @@ public class ContentFieldUtil {
 					dtoConverterContext.isAcceptAllLanguages(),
 					Collections.emptyMap(), dtoConverterRegistry,
 					dtoConverterContext.getHttpServletRequest(), classPK,
-					dtoConverterContext.getLocale(),
-					uriInfoOptional.orElse(null),
+					dtoConverterContext.getLocale(), uriInfo,
 					dtoConverterContext.getUser()));
 		}
 
