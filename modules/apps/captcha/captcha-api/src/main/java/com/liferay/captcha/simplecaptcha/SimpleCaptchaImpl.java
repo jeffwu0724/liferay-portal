db107984253308173358ec2e1d6dccec76441747
Jeff Wu <jeffwu0724@gmail.com>
2024-03-21 09:37:43 -0700

    LPD-1558 add blacklist _maxChallengesBlacklistedPortletNames, the portlets in this blacklist will ignore the max challenges. Captcha checks will always be performed for the associated portlet regardless of the set number of maximum challenges

--- modules/apps/captcha/captcha-api/src/main/java/com/liferay/captcha/simplecaptcha/SimpleCaptchaImpl.java
+++ modules/apps/captcha/captcha-api/src/main/java/com/liferay/captcha/simplecaptcha/SimpleCaptchaImpl.java
@@ -13,10 +13,13 @@ import com.liferay.portal.kernel.captcha.CaptchaTextException;
 import com.liferay.portal.kernel.log.Log;
 import com.liferay.portal.kernel.log.LogFactoryUtil;
 import com.liferay.portal.kernel.model.Release;
+import com.liferay.portal.kernel.portlet.LiferayPortletConfig;
 import com.liferay.portal.kernel.security.RandomUtil;
 import com.liferay.portal.kernel.util.ContentTypes;
+import com.liferay.portal.kernel.util.JavaConstants;
 import com.liferay.portal.kernel.util.ParamUtil;
 import com.liferay.portal.kernel.util.Portal;
+import com.liferay.portal.kernel.util.SetUtil;
 import com.liferay.portal.kernel.util.Validator;
 import com.liferay.portal.kernel.util.WebKeys;
 
@@ -24,6 +27,8 @@ import java.io.IOException;
 import java.io.OutputStream;
 
 import java.util.Map;
+import java.util.Objects;
+import java.util.Set;
 import java.util.concurrent.ConcurrentHashMap;
 
 import javax.portlet.PortletRequest;
@@ -89,6 +94,20 @@ public class SimpleCaptchaImpl implements Captcha {
 
 	@Override
 	public boolean isEnabled(HttpServletRequest httpServletRequest) {
+		LiferayPortletConfig liferayPortletConfig =
+			(LiferayPortletConfig)httpServletRequest.getAttribute(
+				JavaConstants.JAVAX_PORTLET_CONFIG);
+
+		if (liferayPortletConfig != null) {
+			String portletName = liferayPortletConfig.getPortletName();
+
+			for (String name : _maxChallengesBlacklistedPortletNames) {
+				if (Objects.equals(name, portletName)) {
+					return true;
+				}
+			}
+		}
+
 		if (isExceededMaxChallenges(httpServletRequest)) {
 			return false;
 		}
@@ -489,6 +508,13 @@ public class SimpleCaptchaImpl implements Captcha {
 
 	private static final Log _log = LogFactoryUtil.getLog(
 		SimpleCaptchaImpl.class);
+
+	private static final Set<String> _maxChallengesBlacklistedPortletNames =
+		SetUtil.fromArray(
+			new String[] {
+				"com_liferay_server_admin_web_portlet_ServerAdminPortlet",
+				"com_liferay_gogo_shell_web_internal_portlet_GogoShellPortlet"
+			});
 
 	private BackgroundProducer[] _backgroundProducers;
 	private volatile CaptchaConfiguration _captchaConfiguration;
