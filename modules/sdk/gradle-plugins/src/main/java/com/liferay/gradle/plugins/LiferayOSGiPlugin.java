d5233703b2256394ea4473ce9e413c5c63e4be98
Drew Brokke <drew.brokke@liferay.com>
2025-05-16 16:30:01 -0500

    LPD-53767 gradle-plugins: check the compile classpath to see whether javax is required

--- modules/sdk/gradle-plugins/src/main/java/com/liferay/gradle/plugins/LiferayOSGiPlugin.java
+++ modules/sdk/gradle-plugins/src/main/java/com/liferay/gradle/plugins/LiferayOSGiPlugin.java
@@ -805,6 +805,7 @@ public class LiferayOSGiPlugin implements Plugin<Project> {
 									Map<String, String> properties =
 										_getBuilderProperties(
 											project, bundleExtension,
+											javaMainSourceSet,
 											liferayOSGiExtension,
 											buildWSDDTask);
 
@@ -1469,7 +1470,7 @@ public class LiferayOSGiPlugin implements Plugin<Project> {
 
 	private Map<String, String> _getBuilderProperties(
 		Project project, BundleExtension bundleExtension,
-		LiferayOSGiExtension liferayOSGiExtension,
+		SourceSet javaMainSourceSet, LiferayOSGiExtension liferayOSGiExtension,
 		BuildWSDDTask buildWSDDTask) {
 
 		Map<String, String> properties = GradleUtil.toStringMap(
@@ -1506,8 +1507,24 @@ public class LiferayOSGiPlugin implements Plugin<Project> {
 			Constants.BUNDLE_SYMBOLICNAME, bundleSymbolicName + ".wsdd");
 		properties.put(Constants.FRAGMENT_HOST, bundleSymbolicName);
 
-		properties.put(
-			Constants.IMPORT_PACKAGE, "jakarta.servlet,jakarta.servlet.http");
+		String importPackageValue = "jakarta.servlet,jakarta.servlet.http";
+
+		FileCollection compileClasspathFileCollection =
+			javaMainSourceSet.getCompileClasspath();
+
+		FileCollection javaxFileCollection =
+			compileClasspathFileCollection.filter(
+				file -> {
+					String name = file.getName();
+
+					return name.contains("javax");
+				});
+
+		if (!javaxFileCollection.isEmpty()) {
+			importPackageValue = "javax.servlet,javax.servlet.http";
+		}
+
+		properties.put(Constants.IMPORT_PACKAGE, importPackageValue);
 
 		StringBuilder sb = new StringBuilder();
 
