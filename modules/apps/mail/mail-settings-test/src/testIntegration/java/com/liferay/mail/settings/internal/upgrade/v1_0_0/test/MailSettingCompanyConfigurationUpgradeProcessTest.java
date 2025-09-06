da17e5d2a29797618ed29dcd5f0348e39a0f2bea
Jeff Wu <jeffwu0724@gmail.com>
2025-09-05 17:49:37 -0700

    LPD-58857 create update process for MailSettingCompanyConfigurtaion and MailSettingSystemConfigurtaion

--- modules/apps/mail/mail-settings-test/src/testIntegration/java/com/liferay/mail/settings/internal/upgrade/v1_0_0/test/MailSettingCompanyConfigurationUpgradeProcessTest.java
+++ modules/apps/mail/mail-settings-test/src/testIntegration/java/com/liferay/mail/settings/internal/upgrade/v1_0_0/test/MailSettingCompanyConfigurationUpgradeProcessTest.java
@@ -6,10 +6,22 @@
 package com.liferay.mail.settings.internal.upgrade.v1_0_0.test;
 
 import com.liferay.arquillian.extension.junit.bridge.junit.Arquillian;
+import com.liferay.mail.settings.configuration.MailSettingCompanyConfiguration;
+import com.liferay.portal.configuration.module.configuration.ConfigurationProvider;
+import com.liferay.portal.kernel.portlet.PortalPreferences;
+import com.liferay.portal.kernel.portlet.PortletPreferencesFactoryUtil;
+import com.liferay.portal.kernel.service.PortalPreferencesLocalServiceUtil;
 import com.liferay.portal.kernel.test.rule.AggregateTestRule;
+import com.liferay.portal.kernel.test.util.TestPropsValues;
+import com.liferay.portal.kernel.upgrade.UpgradeProcess;
+import com.liferay.portal.kernel.util.PortletKeys;
+import com.liferay.portal.test.rule.Inject;
 import com.liferay.portal.test.rule.LiferayIntegrationTestRule;
+import com.liferay.portal.upgrade.registry.UpgradeStepRegistrator;
+import com.liferay.portal.upgrade.test.util.UpgradeTestUtil;
 
 import org.junit.After;
+import org.junit.Assert;
 import org.junit.Before;
 import org.junit.ClassRule;
 import org.junit.Rule;
@@ -29,14 +41,116 @@ public class MailSettingCompanyConfigurationUpgradeProcessTest {
 
 	@Before
 	public void setUp() throws Exception {
+		_upgradeProcess = UpgradeTestUtil.getUpgradeStep(
+			_upgradeStepRegistrator,
+			"com.liferay.mail.settings.internal.upgrade.v1_0_0." +
+				"MailSettingCompanyConfigurationUpgradeProcess");
+
+		_portalPreferences1 =
+			PortletPreferencesFactoryUtil.getPortalPreferences(
+				TestPropsValues.getUserId(), true);
+
+		_originalPortalPreferencesXML = PortletPreferencesFactoryUtil.toXML(
+			_portalPreferences1);
+
+		_portalPreferences2 =
+			PortletPreferencesFactoryUtil.getPortalPreferences(0L, true);
+
+		_originalPortalPreferencesXML = PortletPreferencesFactoryUtil.toXML(
+			_portalPreferences2);
 	}
 
 	@After
 	public void tearDown() throws Exception {
+		PortalPreferencesLocalServiceUtil.updatePreferences(
+			0L, PortletKeys.PREFS_OWNER_TYPE_COMPANY,
+			_originalPortalPreferencesXML);
+
+		PortalPreferencesLocalServiceUtil.updatePreferences(
+			TestPropsValues.getCompanyId(),
+			PortletKeys.PREFS_OWNER_TYPE_COMPANY,
+			_originalPortalPreferencesXML);
+
+		_configurationProvider.deleteCompanyConfiguration(
+			MailSettingCompanyConfiguration.class,
+			TestPropsValues.getCompanyId());
+		_configurationProvider.deleteSystemConfiguration(
+			MailSettingCompanyConfiguration.class);
 	}
 
 	@Test
-	public void testUpgradeMailSettingCompanyConfiguration() throws Exception {
+	public void testUpgradeMailSettingCompanyConfigurationWithCompanyId()
+		throws Exception {
+
+		_portalPreferences1.setValue(
+			"", "mail.session.mail.pop3.user", "test1");
+		_portalPreferences1.setValue(
+			"", "mail.session.mail.pop3.host", "test1");
+		_portalPreferences1.setValue(
+			"", "mail.session.mail.smtp.starttls.enable", "false");
+
+		PortalPreferencesLocalServiceUtil.updatePreferences(
+			TestPropsValues.getCompanyId(),
+			PortletKeys.PREFS_OWNER_TYPE_COMPANY,
+			PortletPreferencesFactoryUtil.toXML(_portalPreferences1));
+
+		_upgradeProcess.upgrade();
+
+		MailSettingCompanyConfiguration mailSettingCompanyConfiguration =
+			_configurationProvider.getCompanyConfiguration(
+				MailSettingCompanyConfiguration.class,
+				TestPropsValues.getCompanyId());
+
+		Assert.assertFalse(
+			mailSettingCompanyConfiguration.enablePOPServerNotifications());
+		Assert.assertEquals(
+			"test1", mailSettingCompanyConfiguration.popUserName());
+		Assert.assertEquals(
+			"test1", mailSettingCompanyConfiguration.incomingPOPServer());
 	}
+
+	@Test
+	public void testUpgradeMailSettingCompanyConfigurationWithSystemId()
+		throws Exception {
+
+		_portalPreferences2.setValue(
+			"", "mail.session.mail.pop3.user", "test2");
+		_portalPreferences2.setValue(
+			"", "mail.session.mail.pop3.host", "test2");
+		_portalPreferences2.setValue(
+			"", "mail.session.mail.smtp.starttls.enable", "false");
+
+		PortalPreferencesLocalServiceUtil.updatePreferences(
+			0L, PortletKeys.PREFS_OWNER_TYPE_COMPANY,
+			PortletPreferencesFactoryUtil.toXML(_portalPreferences2));
+
+		_upgradeProcess.upgrade();
+
+		MailSettingCompanyConfiguration mailSettingCompanyConfiguration =
+			_configurationProvider.getCompanyConfiguration(
+				MailSettingCompanyConfiguration.class,
+				TestPropsValues.getCompanyId());
+
+		Assert.assertFalse(
+			mailSettingCompanyConfiguration.enablePOPServerNotifications());
+		Assert.assertEquals(
+			"test2", mailSettingCompanyConfiguration.popUserName());
+		Assert.assertEquals(
+			"test2", mailSettingCompanyConfiguration.incomingPOPServer());
+	}
+
+	private static String _originalPortalPreferencesXML;
+
+	@Inject
+	private ConfigurationProvider _configurationProvider;
+
+	private PortalPreferences _portalPreferences1;
+	private PortalPreferences _portalPreferences2;
+	private UpgradeProcess _upgradeProcess;
+
+	@Inject(
+		filter = "(&(component.name=com.liferay.mail.settings.internal.upgrade.registry.MailSettingUpgradeStepRegistrator))"
+	)
+	private UpgradeStepRegistrator _upgradeStepRegistrator;
 
 }
--- modules/apps/mail/mail-settings-test/src/testIntegration/java/com/liferay/mail/settings/internal/upgrade/v1_0_0/test/MailSettingSystemConfigurationUpgradeProcessTest.java
+++ modules/apps/mail/mail-settings-test/src/testIntegration/java/com/liferay/mail/settings/internal/upgrade/v1_0_0/test/MailSettingSystemConfigurationUpgradeProcessTest.java
@@ -6,10 +6,22 @@
 package com.liferay.mail.settings.internal.upgrade.v1_0_0.test;
 
 import com.liferay.arquillian.extension.junit.bridge.junit.Arquillian;
+import com.liferay.mail.settings.configuration.MailSettingSystemConfiguration;
+import com.liferay.portal.configuration.module.configuration.ConfigurationProvider;
+import com.liferay.portal.kernel.portlet.PortalPreferences;
+import com.liferay.portal.kernel.portlet.PortletPreferencesFactoryUtil;
+import com.liferay.portal.kernel.service.PortalPreferencesLocalServiceUtil;
 import com.liferay.portal.kernel.test.rule.AggregateTestRule;
+import com.liferay.portal.kernel.test.util.TestPropsValues;
+import com.liferay.portal.kernel.upgrade.UpgradeProcess;
+import com.liferay.portal.kernel.util.PortletKeys;
+import com.liferay.portal.test.rule.Inject;
 import com.liferay.portal.test.rule.LiferayIntegrationTestRule;
+import com.liferay.portal.upgrade.registry.UpgradeStepRegistrator;
+import com.liferay.portal.upgrade.test.util.UpgradeTestUtil;
 
 import org.junit.After;
+import org.junit.Assert;
 import org.junit.Before;
 import org.junit.ClassRule;
 import org.junit.Rule;
@@ -29,14 +41,61 @@ public class MailSettingSystemConfigurationUpgradeProcessTest {
 
 	@Before
 	public void setUp() throws Exception {
+		_upgradeProcess = UpgradeTestUtil.getUpgradeStep(
+			_upgradeStepRegistrator,
+			"com.liferay.mail.settings.internal.upgrade.v1_0_0." +
+				"MailSettingSystemConfigurationUpgradeProcess");
+
+		_portalPreferences = PortletPreferencesFactoryUtil.getPortalPreferences(
+			TestPropsValues.getUserId(), true);
+
+		_originalPortalPreferencesXML = PortletPreferencesFactoryUtil.toXML(
+			_portalPreferences);
 	}
 
 	@After
 	public void tearDown() throws Exception {
+		PortalPreferencesLocalServiceUtil.updatePreferences(
+			0L, PortletKeys.PREFS_OWNER_TYPE_COMPANY,
+			_originalPortalPreferencesXML);
+		_configurationProvider.deleteSystemConfiguration(
+			MailSettingSystemConfiguration.class);
 	}
 
 	@Test
 	public void testUpgradeMailSettingSystemConfiguration() throws Exception {
+		_portalPreferences.setValue("", "mail.session.mail", "true");
+		_portalPreferences.setValue("", "mail.audit.trail", "test");
+		_portalPreferences.setValue("", "mail.batch.size", "2");
+
+		PortalPreferencesLocalServiceUtil.updatePreferences(
+			0L, PortletKeys.PREFS_OWNER_TYPE_COMPANY,
+			PortletPreferencesFactoryUtil.toXML(_portalPreferences));
+
+		_upgradeProcess.upgrade();
+
+		MailSettingSystemConfiguration mailSettingSystemConfiguration =
+			_configurationProvider.getSystemConfiguration(
+				MailSettingSystemConfiguration.class);
+
+		Assert.assertTrue(mailSettingSystemConfiguration.mailSessionMail());
+		Assert.assertEquals(
+			"test", mailSettingSystemConfiguration.mailAuditTrail());
+		Assert.assertEquals(
+			"2", mailSettingSystemConfiguration.mailBatchSize());
 	}
+
+	private static String _originalPortalPreferencesXML;
+
+	@Inject
+	private ConfigurationProvider _configurationProvider;
+
+	private PortalPreferences _portalPreferences;
+	private UpgradeProcess _upgradeProcess;
+
+	@Inject(
+		filter = "(&(component.name=com.liferay.mail.settings.internal.upgrade.registry.MailSettingUpgradeStepRegistrator))"
+	)
+	private UpgradeStepRegistrator _upgradeStepRegistrator;
 
 }
