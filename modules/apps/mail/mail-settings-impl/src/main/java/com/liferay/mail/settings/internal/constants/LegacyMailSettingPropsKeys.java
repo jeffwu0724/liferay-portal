bd2d9747cad69d42c1b34cb27065b42b0be24f68
Jeff Wu <jeffwu0724@gmail.com>
2025-09-08 14:28:45 -0700

    LPD-58857 create update process for MailSettingCompanyConfigurtaion

---
+++ modules/apps/mail/mail-settings-impl/src/main/java/com/liferay/mail/settings/internal/constants/LegacyMailSettingPropsKeys.java
@@ -0,0 +1,62 @@
+/**
+ * SPDX-FileCopyrightText: (c) 2025 Liferay, Inc. https://liferay.com
+ * SPDX-License-Identifier: LGPL-2.1-or-later OR LicenseRef-Liferay-DXP-EULA-2.0.0-2023-06
+ */
+
+package com.liferay.mail.settings.internal.constants;
+
+/**
+ * @author Jiefeng Wu
+ */
+public class LegacyMailSettingPropsKeys {
+
+	public static final String MAIL_SESSION_MAIL_ADVANCED_PROPERTIES =
+		"mail.session.mail.advanced.properties";
+
+	public static final String MAIL_SESSION_MAIL_POP3_HOST =
+		"mail.session.mail.pop3.host";
+
+	public static final String MAIL_SESSION_MAIL_POP3_PASSWORD =
+		"mail.session.mail.pop3.password";
+
+	public static final String MAIL_SESSION_MAIL_POP3_PORT =
+		"mail.session.mail.pop3.port";
+
+	public static final String MAIL_SESSION_MAIL_POP3_USER =
+		"mail.session.mail.pop3.user";
+
+	public static final String MAIL_SESSION_MAIL_SMTP_HOST =
+		"mail.session.mail.smtp.host";
+
+	public static final String MAIL_SESSION_MAIL_SMTP_PASSWORD =
+		"mail.session.mail.smtp.password";
+
+	public static final String MAIL_SESSION_MAIL_SMTP_PORT =
+		"mail.session.mail.smtp.port";
+
+	public static final String MAIL_SESSION_MAIL_SMTP_STARTTLS_ENABLE =
+		"mail.session.mail.smtp.starttls.enable";
+
+	public static final String MAIL_SESSION_MAIL_SMTP_USER =
+		"mail.session.mail.smtp.user";
+
+	public static final String MAIL_SESSION_MAIL_STORE_PROTOCOL =
+		"mail.session.mail.store.protocol";
+
+	public static final String MAIL_SESSION_MAIL_TRANSPORT_PROTOCOL =
+		"mail.session.mail.transport.protocol";
+
+	public static final String[] MAIL_SETTING_KEYS = {
+		MAIL_SESSION_MAIL_ADVANCED_PROPERTIES, MAIL_SESSION_MAIL_POP3_HOST,
+		MAIL_SESSION_MAIL_POP3_PASSWORD, MAIL_SESSION_MAIL_POP3_PORT,
+		MAIL_SESSION_MAIL_POP3_USER, MAIL_SESSION_MAIL_SMTP_HOST,
+		MAIL_SESSION_MAIL_SMTP_PASSWORD, MAIL_SESSION_MAIL_SMTP_PORT,
+		MAIL_SESSION_MAIL_SMTP_STARTTLS_ENABLE, MAIL_SESSION_MAIL_SMTP_USER,
+		MAIL_SESSION_MAIL_STORE_PROTOCOL, MAIL_SESSION_MAIL_TRANSPORT_PROTOCOL,
+		LegacyMailSettingPropsKeys.POP_SERVER_NOTIFICATIONS_ENABLED
+	};
+
+	public static final String POP_SERVER_NOTIFICATIONS_ENABLED =
+		"pop.server.notifications.enabled";
+
+}
