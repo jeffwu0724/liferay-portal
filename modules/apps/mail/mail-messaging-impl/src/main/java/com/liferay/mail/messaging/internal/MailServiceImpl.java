21b7f5da52e2c7302c982aff24ca2af8a96a62d3
Jeff Wu <jeffwu0724@gmail.com>
2025-09-17 14:08:26 -0700

    LPD-58844 inline _createMailSession()

--- modules/apps/mail/mail-messaging-impl/src/main/java/com/liferay/mail/messaging/internal/MailServiceImpl.java
+++ modules/apps/mail/mail-messaging-impl/src/main/java/com/liferay/mail/messaging/internal/MailServiceImpl.java
@@ -150,7 +150,27 @@ public class MailServiceImpl
 			return session;
 		}
 
-		session = _createMailSession();
+		Properties properties = PropsUtil.getProperties("mail.session.", true);
+
+		String jndiName = properties.getProperty("jndi.name");
+
+		if (Validator.isNotNull(jndiName)) {
+			try {
+				Properties jndiEnvironmentProperties = PropsUtil.getProperties(
+					PropsKeys.JNDI_ENVIRONMENT, true);
+
+				Context context = new InitialContext(jndiEnvironmentProperties);
+
+				session = (Session)JNDIUtil.lookup(context, jndiName);
+			}
+			catch (Exception exception) {
+				_log.error("Unable to lookup " + jndiName, exception);
+			}
+		}
+
+		if (session == null) {
+			session = Session.getInstance(properties);
+		}
 
 		Function<String, String> function =
 			(String key) -> PrefsPropsUtil.getString(
@@ -349,28 +369,6 @@ public class MailServiceImpl
 
 				return null;
 			});
-	}
-
-	private Session _createMailSession() {
-		Properties properties = PropsUtil.getProperties("mail.session.", true);
-
-		String jndiName = properties.getProperty("jndi.name");
-
-		if (Validator.isNotNull(jndiName)) {
-			try {
-				Properties jndiEnvironmentProperties = PropsUtil.getProperties(
-					PropsKeys.JNDI_ENVIRONMENT, true);
-
-				Context context = new InitialContext(jndiEnvironmentProperties);
-
-				return (Session)JNDIUtil.lookup(context, jndiName);
-			}
-			catch (Exception exception) {
-				_log.error("Unable to lookup " + jndiName, exception);
-			}
-		}
-
-		return Session.getInstance(properties);
 	}
 
 	private void _debug(Properties properties) {
