412df4ab01fc4faa164a8e32a7d4b6be625c9f6a
shuyangzhou <shuyang.zhou@liferay.com>
2025-11-08 20:16:49 -0800

    LPD-X This is so wasteful, caused by 8388f775

--- modules/apps/object/object-service/src/main/java/com/liferay/object/service/impl/ObjectEntryLocalServiceImpl.java
+++ modules/apps/object/object-service/src/main/java/com/liferay/object/service/impl/ObjectEntryLocalServiceImpl.java
@@ -2770,6 +2770,9 @@ public class ObjectEntryLocalServiceImpl
 			return dslQuery;
 		}
 
+		ObjectDefinition objectDefinition =
+			_objectDefinitionPersistence.findByPrimaryKey(objectDefinitionId);
+
 		SortDSLQueryVisitor sortDSLQueryVisitor = new SortDSLQueryVisitor(
 			_objectFieldLocalService,
 			_objectRelationshipLocalServiceSnapshot.get());
@@ -2778,9 +2781,7 @@ public class ObjectEntryLocalServiceImpl
 			dslQuery = sortDSLQueryVisitor.visit(
 				dslQuery,
 				new com.liferay.object.internal.sort.Sort(
-					_objectDefinitionPersistence.findByPrimaryKey(
-						objectDefinitionId),
-					sort));
+					objectDefinition, sort));
 		}
 
 		return dslQuery;
