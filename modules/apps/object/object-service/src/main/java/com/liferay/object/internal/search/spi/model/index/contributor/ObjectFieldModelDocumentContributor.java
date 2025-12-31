jeffwu0724/liferay-portal

commit 981b766475bf863bc39966205f0713e672c9d8b6

LPD-73241 Adapt query

modules/apps/object/object-service/src/main/java/com/liferay/object/internal/search/spi/model/index/contributor/ObjectFieldModelDocumentContributor.java
@@ -50,8 +50,11 @@ public class ObjectFieldModelDocumentContributor
 			"objectDefinitionId", objectField.getObjectDefinitionId());
 		document.addKeyword("objectFieldId", objectField.getObjectFieldId());
 		document.addKeyword("state", objectField.isState());
-		document.addKeyword(
-			"unique", _isUnique(objectField.getObjectFieldId()));
+
+		if (objectField.hasUniqueValues()) {
+			document.addKeyword(
+				"unique", _isUnique(objectField.getObjectFieldId()));
+		}
 
 		document.remove(Field.USER_NAME);
 	}
@@ -76,7 +79,9 @@ public class ObjectFieldModelDocumentContributor
 								ObjectFieldSettingConstants.NAME_UNIQUE_VALUES
 							).and(
 								DSLFunctionFactoryUtil.lower(
-									ObjectFieldSettingTable.INSTANCE.value
+									DSLFunctionFactoryUtil.castClobText(
+										ObjectFieldSettingTable.INSTANCE.value
+									)
 								).eq(
 									StringPool.TRUE
 								)

commit 971b52222d0e247f47a5d3fb562ae932d76b882b

LPD-X Fix performance regression from 981b7664

modules/apps/object/object-service/src/main/java/com/liferay/object/internal/search/spi/model/index/contributor/ObjectFieldModelDocumentContributor.java
@@ -5,6 +5,7 @@
 
 package com.liferay.object.internal.search.spi.model.index.contributor;
 
+import com.liferay.object.constants.ObjectFieldConstants;
 import com.liferay.object.constants.ObjectFieldSettingConstants;
 import com.liferay.object.model.ObjectField;
 import com.liferay.object.model.ObjectFieldSetting;
@@ -51,9 +52,11 @@ public class ObjectFieldModelDocumentContributor
 		document.addKeyword("objectFieldId", objectField.getObjectFieldId());
 		document.addKeyword("state", objectField.isState());
 
-		if (objectField.hasUniqueValues()) {
-			document.addKeyword(
-				"unique", _isUnique(objectField.getObjectFieldId()));
+		if (objectField.compareBusinessType(
+				ObjectFieldConstants.BUSINESS_TYPE_AUTO_INCREMENT) ||
+			_isUnique(objectField.getObjectFieldId())) {
+
+			document.addKeyword("unique", true);
 		}
 
 		document.remove(Field.USER_NAME);