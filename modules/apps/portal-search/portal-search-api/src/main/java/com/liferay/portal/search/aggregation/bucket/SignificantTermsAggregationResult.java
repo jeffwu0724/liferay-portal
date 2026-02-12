37a999c740679393112b0cf6b60073d43691f18d
shuyangzhou <shuyang.zhou@liferay.com>
2026-02-12 09:02:45 -0800

    LPD-X Merge SignificantTermsAggregationResult and SignificantTermsAggregationResultImpl into pojo

--- modules/apps/portal-search/portal-search-api/src/main/java/com/liferay/portal/search/aggregation/bucket/SignificantTermsAggregationResult.java
+++ modules/apps/portal-search/portal-search-api/src/main/java/com/liferay/portal/search/aggregation/bucket/SignificantTermsAggregationResult.java
@@ -8,15 +8,26 @@ package com.liferay.portal.search.aggregation.bucket;
 /**
  * @author Michael C. Han
  */
-public abstract class SignificantTermsAggregationResult
-	extends BucketAggregationResult {
+public class SignificantTermsAggregationResult extends BucketAggregationResult {
 
-	public SignificantTermsAggregationResult(String name) {
+	public SignificantTermsAggregationResult(
+		String name, long errorDocCounts, long otherDocCounts) {
+
 		super(name);
+
+		_errorDocCounts = errorDocCounts;
+		_otherDocCounts = otherDocCounts;
 	}
 
-	public abstract long getErrorDocCounts();
+	public long getErrorDocCounts() {
+		return _errorDocCounts;
+	}
 
-	public abstract long getOtherDocCounts();
+	public long getOtherDocCounts() {
+		return _otherDocCounts;
+	}
+
+	private final long _errorDocCounts;
+	private final long _otherDocCounts;
 
 }
