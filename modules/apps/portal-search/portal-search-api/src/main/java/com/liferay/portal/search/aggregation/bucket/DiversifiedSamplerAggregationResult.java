2f6cbbfcf88b021967e643494270f0b8c27c1c1f
shuyangzhou <shuyang.zhou@liferay.com>
2026-02-12 08:49:33 -0800

    LPD-X Merge DiversifiedSamplerAggregationResult and DiversifiedSamplerAggregationResultImpl into pojo

--- modules/apps/portal-search/portal-search-api/src/main/java/com/liferay/portal/search/aggregation/bucket/DiversifiedSamplerAggregationResult.java
+++ modules/apps/portal-search/portal-search-api/src/main/java/com/liferay/portal/search/aggregation/bucket/DiversifiedSamplerAggregationResult.java
@@ -8,7 +8,7 @@ package com.liferay.portal.search.aggregation.bucket;
 /**
  * @author Michael C. Han
  */
-public abstract class DiversifiedSamplerAggregationResult
+public class DiversifiedSamplerAggregationResult
 	extends SamplerAggregationResult {
 
 	public DiversifiedSamplerAggregationResult(String name, long docCount) {
