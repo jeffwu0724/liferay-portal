ef7bd89f48086ecd2f681ab32be0c02bbff932a9
shuyangzhou <shuyang.zhou@liferay.com>
2026-02-12 11:01:18 -0800

    LPD-X Merge HighlightFieldBuilder and HighlightFieldBuilderImpl into pojo

--- modules/apps/portal-search/portal-search-api/src/main/java/com/liferay/portal/search/highlight/HighlightField.java
+++ modules/apps/portal-search/portal-search-api/src/main/java/com/liferay/portal/search/highlight/HighlightField.java
@@ -5,7 +5,6 @@
 
 package com.liferay.portal.search.highlight;
 
-import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 
@@ -13,11 +12,6 @@ import java.util.List;
  * @author Michael C. Han
  */
 public class HighlightField {
-
-	public HighlightField(List<String> fragments, String name) {
-		_fragments = Collections.unmodifiableList(fragments);
-		_name = name;
-	}
 
 	public List<String> getFragments() {
 		return _fragments;
@@ -27,38 +21,9 @@ public class HighlightField {
 		return _name;
 	}
 
-	public static final class HighlightFieldBuilderImpl
-		implements HighlightFieldBuilder {
-
-		@Override
-		public HighlightFieldBuilder addFragment(String fragment) {
-			_fragments.add(fragment);
-
-			return this;
-		}
-
-		@Override
-		public HighlightField build() {
-			return new HighlightField(_fragments, _name);
-		}
-
-		@Override
-		public HighlightFieldBuilder fragments(List<String> fragments) {
-			_fragments = fragments;
-
-			return this;
-		}
-
-		@Override
-		public HighlightFieldBuilder name(String name) {
-			_name = name;
-
-			return this;
-		}
-
-		private List<String> _fragments = new ArrayList<>();
-		private String _name;
-
+	protected HighlightField(List<String> fragments, String name) {
+		_fragments = Collections.unmodifiableList(fragments);
+		_name = name;
 	}
 
 	private final List<String> _fragments;
