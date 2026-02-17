8e8ecabfe50452869bdf97f522773c477dc37640
shuyangzhou <shuyang.zhou@liferay.com>
2026-02-17 13:26:49 -0800

    LPD-X Skip filtering by company when DB partition is enabled, since you won't see other company's data at all, the filter is useless only slowing down the db queries.

--- modules/apps/portal-search/portal-search/src/main/java/com/liferay/portal/search/internal/indexer/IndexerWriterImpl.java
+++ modules/apps/portal-search/portal-search/src/main/java/com/liferay/portal/search/internal/indexer/IndexerWriterImpl.java
@@ -28,6 +28,7 @@ import com.liferay.portal.kernel.util.GetterUtil;
 import com.liferay.portal.kernel.util.ListUtil;
 import com.liferay.portal.kernel.util.PropsKeys;
 import com.liferay.portal.kernel.util.PropsUtil;
+import com.liferay.portal.kernel.util.PropsValues;
 import com.liferay.portal.kernel.workflow.WorkflowConstants;
 import com.liferay.portal.search.batch.BatchIndexingActionable;
 import com.liferay.portal.search.batch.BatchIndexingHelper;
@@ -196,7 +197,9 @@ public class IndexerWriterImpl<T extends BaseModel<?>>
 						BatchIndexingActionable batchIndexingActionable =
 							getBatchIndexingActionable();
 
-						batchIndexingActionable.setCompanyId(companyId);
+						if (!PropsValues.DATABASE_PARTITION_ENABLED) {
+							batchIndexingActionable.setCompanyId(companyId);
+						}
 
 						_modelIndexerWriterContributor.customize(
 							batchIndexingActionable,
