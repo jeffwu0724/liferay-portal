liferay/liferay-portal-cloud-native

commit 8c2c6c89f542c7533df7ca787ccce97428a70af0

LPD-65119 Support removing individual delegates and cleanup empty handlers

modules/apps/export-import/export-import-service/src/main/java/com/liferay/exportimport/internal/data/handler/BatchEnginePortletDataHandlerRegistrar.java
@@ -156,6 +156,8 @@ public class BatchEnginePortletDataHandlerRegistrar {
 	private final List<Long> _enabledCompanyIds = new CopyOnWriteArrayList<>();
 	private volatile ServiceRegistration<FeatureFlagListener>
 		_serviceRegistration;
+	private final Map<String, ServiceRegistration<PortletDataHandler>>
+		_serviceRegistrations = new HashMap<>();
 	private final DCLSingleton
 		<ServiceTrackerList<ServiceRegistration<PortletDataHandler>>>
 			_serviceTrackerListDCLSingleton = new DCLSingleton<>();
@@ -227,21 +229,25 @@ public class BatchEnginePortletDataHandlerRegistrar {
 						"batch.engine.task.item.delegate.name"));
 
 			if (previousBatchEnginePortletDataHandler != null) {
-				return null;
+				return _serviceRegistrations.get(portletId);
 			}
 
-			return _bundleContext.registerService(
-				PortletDataHandler.class, batchEnginePortletDataHandler,
-				_setEnabledCompanyIds(
-					HashMapDictionaryBuilder.<String, Object>put(
-						"batch.engine.task.item.delegate.item.class.name",
-						exportImportDescriptor.getItemClassName()
-					).put(
-						"jakarta.portlet.name",
-						exportImportDescriptor.getPortletId()
-					).put(
-						"service.ranking", Integer.MAX_VALUE
-					).build()));
+			ServiceRegistration<PortletDataHandler> serviceRegistration =
+				_bundleContext.registerService(
+					PortletDataHandler.class, batchEnginePortletDataHandler,
+					_setEnabledCompanyIds(
+						HashMapDictionaryBuilder.<String, Object>put(
+							"batch.engine.task.item.delegate.item.class.name",
+							exportImportDescriptor.getItemClassName()
+						).put(
+							"jakarta.portlet.name", portletId
+						).put(
+							"service.ranking", Integer.MAX_VALUE
+						).build()));
+
+			_serviceRegistrations.put(portletId, serviceRegistration);
+
+			return serviceRegistration;
 		}
 
 		@Override
@@ -257,20 +263,44 @@ public class BatchEnginePortletDataHandlerRegistrar {
 				serviceReference,
 			ServiceRegistration<PortletDataHandler> serviceRegistration) {
 
-			PortletDataHandler portletDataHandler = _bundleContext.getService(
-				serviceRegistration.getReference());
+			ExportImportVulcanBatchEngineTaskItemDelegate<?>
+				exportImportVulcanBatchEngineTaskItemDelegate =
+					(ExportImportVulcanBatchEngineTaskItemDelegate<?>)
+						_bundleContext.getService(serviceReference);
 
-			if (portletDataHandler instanceof
-					BatchEnginePortletDataHandler
-						batchEnginePortletDataHandler) {
+			ExportImportVulcanBatchEngineTaskItemDelegate.ExportImportDescriptor
+				exportImportDescriptor =
+					exportImportVulcanBatchEngineTaskItemDelegate.
+						getExportImportDescriptor();
 
-				// TODO remove only if empty
+			String portletId = exportImportDescriptor.getPortletId();
 
-				_batchEnginePortletDataHandlers.remove(
-					batchEnginePortletDataHandler.getPortletId());
+			BatchEnginePortletDataHandler batchEnginePortletDataHandler =
+				_batchEnginePortletDataHandlers.get(portletId);
+
+			if (batchEnginePortletDataHandler == null) {
+				return;
 			}
 
-			serviceRegistration.unregister();
+			String className = GetterUtil.getObject(
+				(String)serviceReference.getProperty(
+					"batch.engine.task.item.delegate.class.name"),
+				() -> (String)serviceReference.getProperty(
+					"batch.engine.entity.class.name"));
+
+			String taskItemDelegateName = (String)serviceReference.getProperty(
+				"batch.engine.task.item.delegate.name");
+
+			batchEnginePortletDataHandler.
+				unregisterExportImportVulcanBatchEngineTaskItemDelegate(
+					className, taskItemDelegateName);
+
+			String[] classNames = batchEnginePortletDataHandler.getClassNames();
+
+			if (classNames.length == 0) {
+				serviceRegistration.unregister();
+				_batchEnginePortletDataHandlers.remove(portletId);
+			}
 		}
 
 		private final BundleContext _bundleContext;

modules/apps/export-import/export-import-service/src/main/java/com/liferay/exportimport/internal/data/handler/BatchEnginePortletDataHandler.java
@@ -53,8 +53,10 @@ import java.io.Serializable;
 
 import java.util.ArrayList;
 import java.util.Collections;
+import java.util.Iterator;
 import java.util.List;
 import java.util.Map;
+import java.util.Objects;
 import java.util.zip.ZipEntry;
 import java.util.zip.ZipOutputStream;
 
@@ -202,17 +204,28 @@ public class BatchEnginePortletDataHandler extends BasePortletDataHandler {
 			setDataLevel(DataLevel.SITE);
 		}
 
-		if (_registrations.size() > 1) {
-			setExportControls(
-				TransformUtil.transformToArray(
-					_registrations, this::_getPortletDataHandlerControl,
-					PortletDataHandlerControl.class));
+		_updateExportControls();
+	}
 
-			setEmptyControlsAllowed(false);
-		}
-		else {
-			setEmptyControlsAllowed(true);
-			setExportControls();
+	public void unregisterExportImportVulcanBatchEngineTaskItemDelegate(
+		String className, String taskItemDelegateName) {
+
+		Iterator<Registration> iterator = _registrations.iterator();
+
+		while (iterator.hasNext()) {
+			Registration registration = iterator.next();
+
+			if (Objects.equals(registration.getClassName(), className) &&
+				Objects.equals(
+					registration.getTaskItemDelegateName(),
+					taskItemDelegateName)) {
+
+				iterator.remove();
+
+				_updateExportControls();
+
+				return;
+			}
 		}
 	}
 
@@ -524,6 +537,21 @@ public class BatchEnginePortletDataHandler extends BasePortletDataHandler {
 			StringPool.FORWARD_SLASH, ExportImportPathUtil.PATH_PREFIX_GROUP,
 			StringPool.FORWARD_SLASH, groupId, StringPool.FORWARD_SLASH,
 			fileName);
+	}
+
+	private void _updateExportControls() {
+		if (_registrations.size() > 1) {
+			setExportControls(
+				TransformUtil.transformToArray(
+					_registrations, this::_getPortletDataHandlerControl,
+					PortletDataHandlerControl.class));
+
+			setEmptyControlsAllowed(false);
+		}
+		else {
+			setEmptyControlsAllowed(true);
+			setExportControls();
+		}
 	}
 
 	private final BatchEngineExportTaskExecutor _batchEngineExportTaskExecutor;

jeffwu0724/liferay-portal

commit 9a1a36b2b5e3488aba8bc1245035fc143b3fd2d7

LPD-X Fix random startup failure. Caused by 8c2c6c89f542c7533df7ca787ccce97428a70af0
2026-03-18 09:17:08.950 ERROR [Framework Event Dispatcher: Equinox Container: 7d93bb00-6d71-4ef0-865c-920cb9ef1b7f][Framework:47] FrameworkEvent ERROR
java.util.ConcurrentModificationException: null
	at java.util.HashMap.computeIfAbsent(HashMap.java:1229) ~[?:?]
	at com.liferay.exportimport.internal.data.handler.BatchEnginePortletDataHandlerRegistrar$VulcanBatchEngineTaskItemDelegateServiceTrackerCustomizer.addingService(BatchEnginePortletDataHandlerRegistrar.java:188) ~[?:?]
	at com.liferay.exportimport.internal.data.handler.BatchEnginePortletDataHandlerRegistrar$VulcanBatchEngineTaskItemDelegateServiceTrackerCustomizer.addingService(BatchEnginePortletDataHandlerRegistrar.java:118) ~[?:?]
	at com.liferay.osgi.service.tracker.collections.internal.list.ServiceTrackerListImpl$ServiceReferenceServiceTrackerCustomizer.addingService(ServiceTrackerListImpl.java:155) ~[com.liferay.osgi.service.tracker.collections.jar:?]

modules/apps/export-import/export-import-service/src/main/java/com/liferay/exportimport/internal/data/handler/BatchEnginePortletDataHandlerRegistrar.java
@@ -27,8 +27,8 @@ import com.liferay.portal.vulcan.batch.engine.VulcanBatchEngineTaskItemDelegate;
 import com.liferay.staging.StagingGroupHelper;
 
 import java.util.Dictionary;
-import java.util.HashMap;
 import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
 
 import org.osgi.framework.BundleContext;
 import org.osgi.framework.ServiceReference;
@@ -107,7 +107,7 @@ public class BatchEnginePortletDataHandlerRegistrar {
 	private LayoutLocalService _layoutLocalService;
 
 	private final Map<String, ServiceRegistration<PortletDataHandler>>
-		_portletIdServiceRegistrations = new HashMap<>();
+		_portletIdServiceRegistrations = new ConcurrentHashMap<>();
 	private final DCLSingleton
 		<ServiceTrackerList<ServiceRegistration<PortletDataHandler>>>
 			_serviceTrackerListDCLSingleton = new DCLSingleton<>();