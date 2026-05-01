cbbd3ca845e421e9d74a587f13d87c05045cdc8b
Jeff Wu <jeffwu0724@gmail.com>
2026-05-01 16:49:41 -0700

    LPD-x add test to check the number of messages sent with only EntityCache

--- modules/apps/portal-cache/portal-cache-test/src/testIntegration/java/com/liferay/portal/cache/internal/test/ClusterCacheReplicationTest.java
+++ modules/apps/portal-cache/portal-cache-test/src/testIntegration/java/com/liferay/portal/cache/internal/test/ClusterCacheReplicationTest.java
@@ -6,19 +6,25 @@
 package com.liferay.portal.cache.internal.test;
 
 import com.liferay.arquillian.extension.junit.bridge.junit.Arquillian;
+import com.liferay.petra.string.StringBundler;
+import com.liferay.portal.cache.PortalCacheReplicator;
+import com.liferay.portal.cache.PortalCacheWrapper;
 import com.liferay.portal.kernel.cache.PortalCache;
 import com.liferay.portal.kernel.cache.PortalCacheException;
 import com.liferay.portal.kernel.cache.PortalCacheHelperUtil;
 import com.liferay.portal.kernel.cache.PortalCacheListener;
 import com.liferay.portal.kernel.cache.PortalCacheListenerScope;
 import com.liferay.portal.kernel.cache.PortalCacheManagerNames;
+import com.liferay.portal.kernel.dao.orm.EntityCacheUtil;
 import com.liferay.portal.kernel.dao.orm.QueryUtil;
 import com.liferay.portal.kernel.model.UserGroup;
 import com.liferay.portal.kernel.service.UserGroupLocalServiceUtil;
 import com.liferay.portal.kernel.test.ReflectionTestUtil;
 import com.liferay.portal.kernel.test.rule.TomcatClusterTestRule;
+import com.liferay.portal.kernel.test.util.RandomTestUtil;
 import com.liferay.portal.kernel.test.util.TestPropsValues;
 import com.liferay.portal.kernel.test.util.UserGroupTestUtil;
+import com.liferay.portal.model.impl.UserGroupImpl;
 import com.liferay.portal.test.cluster.tomcat.TomcatCluster;
 import com.liferay.portal.test.cluster.tomcat.TomcatNode;
 import com.liferay.portal.test.rule.LiferayIntegrationTestRule;
@@ -30,6 +36,7 @@ import java.util.List;
 import java.util.Objects;
 import java.util.concurrent.ConcurrentMap;
 import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.atomic.AtomicInteger;
 import java.util.function.Function;
 
 import org.junit.After;
@@ -300,6 +307,70 @@ public class ClusterCacheReplicationTest implements Serializable {
 		userGroups = _tomcatNode1.syncExecute(getUserGroupsClusterExecutable);
 
 		Assert.assertTrue(userGroups.toString(), userGroups.isEmpty());
+	}
+
+	@Test
+	public void testEntityCacheReplicationMessageCount() throws Exception {
+		_tomcatNode1.syncExecute(
+			() -> {
+				EntityCacheUtil.clearCache(UserGroupImpl.class);
+
+				PortalCache<Serializable, Serializable> portalCache =
+					_getEntityEhcachePortalCache(UserGroupImpl.class);
+
+				portalCache.registerPortalCacheListener(
+					new TestPortalCacheReplicator());
+
+				return null;
+			});
+
+		_tomcatNode1.syncExecute(
+			() -> {
+				PortalCache<Serializable, Serializable> portalCache =
+					_getEntityEhcachePortalCache(UserGroupImpl.class);
+
+				long testKey = RandomTestUtil.randomLong();
+
+				portalCache.put(testKey, "test1");
+				portalCache.put(testKey, "test2");
+				portalCache.remove(testKey);
+
+				return null;
+			});
+
+		int[] counts = _tomcatNode1.syncExecute(
+			() -> {
+				PortalCache<Serializable, Serializable> portalCache =
+					_getEntityEhcachePortalCache(UserGroupImpl.class);
+
+				TestPortalCacheReplicator testPortalCacheReplicator =
+					(TestPortalCacheReplicator)_getListenerOnEhcachePortalCache(
+						TestPortalCacheReplicator.class.getName(), portalCache);
+
+				int[] result = {
+					testPortalCacheReplicator.getPutCount(),
+					testPortalCacheReplicator.getUpdateCount(),
+					testPortalCacheReplicator.getRemoveCount(),
+					testPortalCacheReplicator.getRemoveAllCount()
+				};
+
+				portalCache.unregisterPortalCacheListener(
+					testPortalCacheReplicator);
+
+				EntityCacheUtil.clearCache(UserGroupImpl.class);
+
+				return result;
+			});
+
+		System.out.println(
+			StringBundler.concat(
+				"!!!!!!!!!!! events on node 1: put=", counts[0], ", update=",
+				counts[1], ", remove=", counts[2], ", removeAll=", counts[3]));
+
+		Assert.assertEquals("put", 1, counts[0]);
+		Assert.assertEquals("update", 1, counts[1]);
+		Assert.assertEquals("remove", 1, counts[2]);
+		Assert.assertEquals("removeAll", 0, counts[3]);
 	}
 
 	@Test
@@ -796,11 +867,140 @@ public class ClusterCacheReplicationTest implements Serializable {
 
 	}
 
+	public static class TestPortalCacheReplicator
+		implements PortalCacheReplicator<Serializable, Serializable>,
+				   Serializable {
+
+		@Override
+		public void dispose() {
+		}
+
+		public int getPutCount() {
+			return _putCount.get();
+		}
+
+		public int getRemoveAllCount() {
+			return _removeAllCount.get();
+		}
+
+		public int getRemoveCount() {
+			return _removeCount.get();
+		}
+
+		public int getUpdateCount() {
+			return _updateCount.get();
+		}
+
+		@Override
+		public void notifyEntryEvicted(
+				PortalCache<Serializable, Serializable> portalCache,
+				Serializable key, Serializable value, int timeToLive)
+			throws PortalCacheException {
+		}
+
+		@Override
+		public void notifyEntryExpired(
+				PortalCache<Serializable, Serializable> portalCache,
+				Serializable key, Serializable value, int timeToLive)
+			throws PortalCacheException {
+		}
+
+		@Override
+		public void notifyEntryPut(
+				PortalCache<Serializable, Serializable> portalCache,
+				Serializable key, Serializable value, int timeToLive)
+			throws PortalCacheException {
+
+			_putCount.incrementAndGet();
+		}
+
+		@Override
+		public void notifyEntryRemoved(
+				PortalCache<Serializable, Serializable> portalCache,
+				Serializable key, Serializable value, int timeToLive)
+			throws PortalCacheException {
+
+			_removeCount.incrementAndGet();
+		}
+
+		@Override
+		public void notifyEntryUpdated(
+				PortalCache<Serializable, Serializable> portalCache,
+				Serializable key, Serializable value, int timeToLive)
+			throws PortalCacheException {
+
+			_updateCount.incrementAndGet();
+		}
+
+		@Override
+		public void notifyRemoveAll(
+				PortalCache<Serializable, Serializable> portalCache)
+			throws PortalCacheException {
+
+			_removeAllCount.incrementAndGet();
+		}
+
+		private final AtomicInteger _putCount = new AtomicInteger();
+		private final AtomicInteger _removeAllCount = new AtomicInteger();
+		private final AtomicInteger _removeCount = new AtomicInteger();
+		private final AtomicInteger _updateCount = new AtomicInteger();
+
+	}
+
 	private static PortalCacheListener<?, ?> _getPortalCacheListener(
 		String className, PortalCache<?, ?> portalCache) {
 
 		portalCache = ReflectionTestUtil.getFieldValue(
 			portalCache, "_portalCache");
+
+		Object aggregatedPortalCacheListener = ReflectionTestUtil.getFieldValue(
+			portalCache, "aggregatedPortalCacheListener");
+
+		ConcurrentMap<PortalCacheListener<?, ?>, PortalCacheListenerScope>
+			portalCacheListeners = ReflectionTestUtil.getFieldValue(
+				aggregatedPortalCacheListener, "_portalCacheListeners");
+
+		for (PortalCacheListener<?, ?> portalCacheListener :
+				portalCacheListeners.keySet()) {
+
+			Class<?> clazz = portalCacheListener.getClass();
+
+			if (Objects.equals(clazz.getName(), className)) {
+				return portalCacheListener;
+			}
+		}
+
+		throw new IllegalStateException(className + " does not exist");
+	}
+
+	@SuppressWarnings("unchecked")
+	private PortalCache<Serializable, Serializable>
+		_getEntityEhcachePortalCache(Class<?> modelImplClass) {
+
+		PortalCache<?, ?> portalCache = EntityCacheUtil.getPortalCache(
+			modelImplClass);
+
+		if (Objects.equals(
+				portalCache.getClass(
+				).getName(),
+				_CLASS_NAME_CT_AWARE_PORTAL_CACHE)) {
+
+			portalCache = ReflectionTestUtil.getFieldValue(
+				portalCache, "_productionPortalCache");
+		}
+
+		while (portalCache instanceof PortalCacheWrapper) {
+			PortalCacheWrapper<?, ?> portalCacheWrapper =
+				(PortalCacheWrapper<?, ?>)portalCache;
+
+			portalCache = portalCacheWrapper.getWrappedPortalCache();
+		}
+
+		return (PortalCache<Serializable, Serializable>)portalCache;
+	}
+
+	private PortalCacheListener<?, ?> _getListenerOnEhcachePortalCache(
+		String className, PortalCache<?, ?> portalCache) {
 
 		Object aggregatedPortalCacheListener = ReflectionTestUtil.getFieldValue(
 			portalCache, "aggregatedPortalCacheListener");
@@ -832,6 +1032,9 @@ public class ClusterCacheReplicationTest implements Serializable {
 				"_portalCacheReplicator"),
 			fieldName, fieldValue);
 	}
+
+	private static final String _CLASS_NAME_CT_AWARE_PORTAL_CACHE =
+		"com.liferay.portal.cache.internal.dao.orm.CTAwarePortalCache";
 
 	private static final String _CLASS_NAME_EHCACHE_PORTAL_CACHE_REPLICATOR =
 		"com.liferay.portal.cache.ehcache.internal.events." +
