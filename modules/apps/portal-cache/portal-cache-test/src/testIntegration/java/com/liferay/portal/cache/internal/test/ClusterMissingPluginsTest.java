efdf31eb79bfdb186a67111160dd9e186226ffdd
Jeff Wu <jeffwu0724@gmail.com>
2026-05-26 17:59:28 -0700

    LPD-90676 Migrate ScheduleJobOnClusterNode1 to an integration test

---
+++ modules/apps/portal-cache/portal-cache-test/src/testIntegration/java/com/liferay/portal/cache/internal/test/ClusterMissingPluginsTest.java
@@ -0,0 +1,219 @@
+/**
+ * SPDX-FileCopyrightText: (c) 2026 Liferay, Inc. https://liferay.com
+ * SPDX-License-Identifier: LGPL-2.1-or-later OR LicenseRef-Liferay-DXP-EULA-2.0.0-2023-06
+ */
+
+package com.liferay.portal.cache.internal.test;
+
+import com.liferay.arquillian.extension.junit.bridge.junit.Arquillian;
+import com.liferay.petra.function.UnsafeRunnable;
+import com.liferay.portal.kernel.cluster.ClusterMasterExecutorUtil;
+import com.liferay.portal.kernel.cluster.ClusterMasterTokenTransitionListener;
+import com.liferay.portal.kernel.module.util.SystemBundleUtil;
+import com.liferay.portal.kernel.scheduler.SchedulerEngineHelperUtil;
+import com.liferay.portal.kernel.scheduler.SchedulerJobConfiguration;
+import com.liferay.portal.kernel.scheduler.StorageType;
+import com.liferay.portal.kernel.scheduler.TimeUnit;
+import com.liferay.portal.kernel.scheduler.TriggerConfiguration;
+import com.liferay.portal.kernel.test.rule.TomcatClusterTestRule;
+import com.liferay.portal.test.cluster.tomcat.TomcatCluster;
+import com.liferay.portal.test.cluster.tomcat.TomcatNode;
+import com.liferay.portal.test.log.LogCapture;
+import com.liferay.portal.test.log.LoggerTestUtil;
+import com.liferay.portal.test.rule.LiferayIntegrationTestRule;
+
+import java.io.Serializable;
+
+import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.Future;
+
+import org.junit.After;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.ClassRule;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import org.osgi.framework.BundleContext;
+import org.osgi.framework.ServiceRegistration;
+
+/**
+ * @author Jiefeng Wu
+ */
+@RunWith(Arquillian.class)
+public class ClusterMissingPluginsTest implements Serializable {
+
+	@ClassRule
+	@Rule
+	public static final LiferayIntegrationTestRule liferayIntegrationTestRule =
+		new LiferayIntegrationTestRule();
+
+	@ClassRule
+	public static final TomcatClusterTestRule tomcatClusterTestRule =
+		new TomcatClusterTestRule();
+
+	@Before
+	public void setUp() throws Exception {
+		TomcatCluster.Builder builder1 =
+			tomcatClusterTestRule.buildTomcatNode();
+
+		_tomcatNode1 = builder1.build();
+
+		_tomcatNode1.start(true);
+
+		TomcatCluster.Builder builder2 =
+			tomcatClusterTestRule.buildTomcatNode();
+
+		_tomcatNode2 = builder2.build();
+
+		_tomcatNode2.start(true);
+	}
+
+	@After
+	public void tearDown() throws Exception {
+		_tomcatNode1.stop();
+
+		_tomcatNode2.stop();
+	}
+
+	@Test
+	public void testScheduleJobOnClusterNode1() throws Exception {
+		String jobName = TestSchedulerJobConfiguration.class.getName();
+
+		TomcatNode masterTomcatNode = _tomcatNode1;
+		TomcatNode slaveTomcatNode = _tomcatNode2;
+
+		if (!_tomcatNode1.syncExecute(ClusterMasterExecutorUtil::isMaster)) {
+			masterTomcatNode = _tomcatNode2;
+			slaveTomcatNode = _tomcatNode1;
+		}
+
+		masterTomcatNode.syncExecute(
+			() -> {
+				TestSchedulerJobConfiguration.registerAndAwaitExecution();
+
+				return null;
+			});
+
+		Future<?> future = slaveTomcatNode.execute(
+			() -> {
+				TestClusterMasterTokenTransitionListener.
+					registerAndAwaitMasterToken();
+
+				return null;
+			});
+
+		masterTomcatNode.stop();
+
+		future.get();
+
+		Assert.assertTrue(
+			slaveTomcatNode.syncExecute(ClusterMasterExecutorUtil::isMaster));
+
+		Assert.assertTrue(
+			slaveTomcatNode.syncExecute(
+				() -> {
+					try (LogCapture logCapture =
+							LoggerTestUtil.configureLog4JLogger(
+								"com.liferay.portal.scheduler.quartz." +
+									"internal.QuartzSchedulerEngine",
+								LoggerTestUtil.WARN)) {
+
+						TestSchedulerJobConfiguration.
+							registerAndAwaitExecution();
+
+						for (String message : logCapture.getMessages()) {
+							if (message.contains(
+									TestSchedulerJobConfiguration.class.
+										getName()) &&
+								message.contains("already exists")) {
+
+								return true;
+							}
+						}
+
+						return false;
+					}
+				}));
+
+		Assert.assertNotNull(
+			slaveTomcatNode.syncExecute(
+				() -> SchedulerEngineHelperUtil.getScheduledJob(
+					jobName, jobName, StorageType.MEMORY_CLUSTERED)));
+	}
+
+	private transient TomcatNode _tomcatNode1;
+	private transient TomcatNode _tomcatNode2;
+
+	private static class TestClusterMasterTokenTransitionListener
+		implements ClusterMasterTokenTransitionListener {
+
+		public static void registerAndAwaitMasterToken() throws Exception {
+			BundleContext bundleContext = SystemBundleUtil.getBundleContext();
+
+			TestClusterMasterTokenTransitionListener
+				testClusterMasterTokenTransitionListener =
+					new TestClusterMasterTokenTransitionListener();
+
+			ServiceRegistration<?> serviceRegistration =
+				bundleContext.registerService(
+					ClusterMasterTokenTransitionListener.class,
+					testClusterMasterTokenTransitionListener, null);
+
+			CountDownLatch countDownLatch =
+				testClusterMasterTokenTransitionListener._countDownLatch;
+
+			countDownLatch.await();
+
+			serviceRegistration.unregister();
+		}
+
+		@Override
+		public void masterTokenAcquired() {
+			_countDownLatch.countDown();
+		}
+
+		@Override
+		public void masterTokenReleased() {
+		}
+
+		private final CountDownLatch _countDownLatch = new CountDownLatch(1);
+
+	}
+
+	private static class TestSchedulerJobConfiguration
+		implements SchedulerJobConfiguration {
+
+		public static void registerAndAwaitExecution() throws Exception {
+			BundleContext bundleContext = SystemBundleUtil.getBundleContext();
+
+			TestSchedulerJobConfiguration testSchedulerJobConfiguration =
+				new TestSchedulerJobConfiguration();
+
+			bundleContext.registerService(
+				SchedulerJobConfiguration.class, testSchedulerJobConfiguration,
+				null);
+
+			CountDownLatch countDownLatch =
+				testSchedulerJobConfiguration._countDownLatch;
+
+			countDownLatch.await();
+		}
+
+		@Override
+		public UnsafeRunnable<Exception> getJobExecutorUnsafeRunnable() {
+			return _countDownLatch::countDown;
+		}
+
+		@Override
+		public TriggerConfiguration getTriggerConfiguration() {
+			return TriggerConfiguration.createTriggerConfiguration(
+				5, TimeUnit.SECOND);
+		}
+
+		private final CountDownLatch _countDownLatch = new CountDownLatch(1);
+
+	}
+
+}
